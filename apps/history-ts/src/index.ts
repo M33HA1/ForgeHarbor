221bab9718272b7affc99918433aa29b7a3eb4e8
Meehal <meehal.malik@gmail.com>
2025-09-11 01:24:31 +0530

    kong api

--- apps/history-ts/src/index.ts
+++ apps/history-ts/src/index.ts
@@ -2,6 +2,11 @@ import Fastify from "fastify";
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from 'url';
+
+// Fix for ES modules __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, "../../config/.env") });
 
